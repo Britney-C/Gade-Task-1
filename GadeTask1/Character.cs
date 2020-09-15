563674584c9848546d84f3e4ff75f4547b57fad9
Britney Carstens <britanani@gmail.com>
2020-09-15 11:37:49 +0200

    Initial commit

---
+++ GadeTask1/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GadeTask1
+{
+	abstract class Character : Tile
+	{
+		protected int HP;
+		protected int maxHP;
+		protected int damage;
+
+		enum Movement
+		{
+			No_movement,
+			Up,
+			Down,
+			Left,
+			Right
+		}
+
+		public int accessHP { get; set; }
+		public int accessMaxHP { get; set; }
+		public int accessDamage { get; set; }
+	}
+}
---
+++ GadeTask1/Tile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GadeTask1
+{
+	abstract class Tile
+	{
+		protected int X { get; set; }
+		protected int Y { get; set; }
+
+		public enum TileType
+		{
+			Hero = 0,
+			Enemy = 1,
+			Gold = 2,
+			Weapon = 3
+		}
+
+		public Tile()
+		{
+			X = 0;
+			Y = 0;
+			char hero = 'H';
+			char enemy = 'E';
+			char gold = 'G';
+			char weapon = 'W';
+			hero = (char)TileType.Hero;
+			enemy = (char)TileType.Enemy;
+			gold = (char)TileType.Gold;
+			weapon = (char)TileType.Weapon;
+		}
+
+		 class Obstacle : Tile
+		{
+			public Obstacle(int xPosition, int yPosition, char symbol)
+			{
+				xPosition = base.X;
+				yPosition = base.Y;
+			}
+		}
+
+		public class EmptyTile : Tile
+		{
+			public EmptyTile(int xPosition, int yPosition)
+			{
+				Console.WriteLine(".");
+
+				xPosition = base.X;
+				yPosition = base.Y;
+			}
+		}
+	}
+}
+}
