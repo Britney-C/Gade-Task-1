bc934050c84c77b42388bb89436f917906e41f6f
Britney Carstens <britanani@gmail.com>
2020-09-15 12:58:06 +0200

    Started adding the Goblin class.

---
+++ GadeTask1/Goblin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GadeTask1
+{
+    class Goblin : Enemy
+    {
+        //Question 2.5 : Concrete Goblin class which inherits Enemy.
+
+        protected int[,] enemyVisionArray = { };
+
+        public Goblin(int _X, int _Y, int enemyHP = 10, int enemyDamage = 1) : base(enemyHP, enemyDamage, _X, _Y, "Goblin", 'G')
+        {
+        }
+
+        public virtual Tile ReturnMove(Movement random)
+        {
+            return null;
+        }
+
+        //public override string ReturnMove()
+    }
+}
